06f098a69e295f4e08ca43aa3ce6be6d2e098467
yukaorange <takaoka.webdesigner@gmail.com>
2024-02-18 11:33:01 +0900

    test

--- vite.config.js
+++ vite.config.js
@@ -23,68 +23,68 @@ const readConfigJSONFile = filePath => {
 
 const root = resolve(__dirname, 'src/pages')
 
-export default defineConfig(async () => {
-  // ビルド対象のHTMLファイルをリスト化する
-  const rollupOptionsInput = await rollupFiles('./src/pages', '.html', {})
-  // サイト設定のJSONファイルを読み込む
-  configs.global = await readConfigJSONFile('./src/configs/global.json')
-  configs.meta = await readConfigJSONFile('./src/configs/meta.json')
+// export default defineConfig(async () => {
+//   // ビルド対象のHTMLファイルをリスト化する
+//   const rollupOptionsInput = await rollupFiles('./src/pages', '.html', {})
+//   // サイト設定のJSONファイルを読み込む
+//   configs.global = await readConfigJSONFile('./src/configs/global.json')
+//   configs.meta = await readConfigJSONFile('./src/configs/meta.json')
 
-  return {
-    root: root,
-    base: '/',
-    publicDir: resolve(__dirname, 'public'),
-    server: {
-      port: 8080
-    },
-    build: {
-      outDir: resolve(__dirname, 'dist'),
-      rollupOptions: {
-        input: rollupOptionsInput
-      },
-      css: {
-        devSourcemap: true, 
-        postcss: {
-          // .browserslistrcで指定したブラウザ用にCSSを自動で調整してくれる
-          plugins: [autoprefixer()]
-        }
-      }
-    },
-    plugins: [
-      // HTMLソースを minify する。
-      // ◆ build -> minifyした後にprettierで整形（WordPress等の作業をしやすくする目的）
-      // ◆ production -> minifyだけを行う（静的サイトとしてデプロイを前提とするため。）
-      ViteMinifyPlugin(),
-      // 共通パーツを読み込むための記述
-      // partialDirectoryで指定したディレクトリのパーツが読み込まれる。
-      handlebars({
-        partialDirectory: [
-          resolve(__dirname, 'src/includes/globals'),
-          resolve(__dirname, 'src/includes/components'),
-          resolve(__dirname, 'src/includes/modules')
-        ],
-        // /src/configs に格納のJSON設定ファイルを用いてページ毎に使える変数を適用
-        // 詳しくは https://handlebarsjs.com/ をご覧ください。
-        context(pagePath) {
-          return {
-            ...configs.global,
-            page:
-              typeof configs.meta[pagePath] !== 'undefined' &&
-              configs.meta[pagePath],
-            pagePath: pagePath
-          }
-        }
-      }),
-      ViteImageOptimizer(configs.global.image.optimization),
-      glsl()
-    ],
-    resolve: {
-      // src/assets/scss を @ として読み込むことができる。
-      // SCSSファイルで外部ファイルを読み込むときに活用すると便利。
-      alias: {
-        '@': resolve(__dirname, 'src/assets/scss'),
-        '@js': resolve(__dirname, 'src/assets/js')
-      }
-    }
-  }
-})
+//   return {
+//     root: root,
+//     base: '/',
+//     publicDir: resolve(__dirname, 'public'),
+//     server: {
+//       port: 8080
+//     },
+//     build: {
+//       outDir: resolve(__dirname, 'dist'),
+//       rollupOptions: {
+//         input: rollupOptionsInput
+//       },
+//       css: {
+//         devSourcemap: true, 
+//         postcss: {
+//           // .browserslistrcで指定したブラウザ用にCSSを自動で調整してくれる
+//           plugins: [autoprefixer()]
+//         }
+//       }
+//     },
+//     plugins: [
+//       // HTMLソースを minify する。
+//       // ◆ build -> minifyした後にprettierで整形（WordPress等の作業をしやすくする目的）
+//       // ◆ production -> minifyだけを行う（静的サイトとしてデプロイを前提とするため。）
+//       ViteMinifyPlugin(),
+//       // 共通パーツを読み込むための記述
+//       // partialDirectoryで指定したディレクトリのパーツが読み込まれる。
+//       handlebars({
+//         partialDirectory: [
+//           resolve(__dirname, 'src/includes/globals'),
+//           resolve(__dirname, 'src/includes/components'),
+//           resolve(__dirname, 'src/includes/modules')
+//         ],
+//         // /src/configs に格納のJSON設定ファイルを用いてページ毎に使える変数を適用
+//         // 詳しくは https://handlebarsjs.com/ をご覧ください。
+//         context(pagePath) {
+//           return {
+//             ...configs.global,
+//             page:
+//               typeof configs.meta[pagePath] !== 'undefined' &&
+//               configs.meta[pagePath],
+//             pagePath: pagePath
+//           }
+//         }
+//       }),
+//       ViteImageOptimizer(configs.global.image.optimization),
+//       glsl()
+//     ],
+//     resolve: {
+//       // src/assets/scss を @ として読み込むことができる。
+//       // SCSSファイルで外部ファイルを読み込むときに活用すると便利。
+//       alias: {
+//         '@': resolve(__dirname, 'src/assets/scss'),
+//         '@js': resolve(__dirname, 'src/assets/js')
+//       }
+//     }
+//   }
+// })
