781de6f3ed4d71cd9a6231db53e822bd0c480cba
yukaorange <takaoka.webdesigner@gmail.com>
2024-02-18 12:51:11 +0900

    Update base URL and optimize HTML structure

--- vite.config.js
+++ vite.config.js
@@ -33,7 +33,7 @@ export default defineConfig(async () => {
 
   return {
     root: root,
-    base: '/',
+    base: './',
     publicDir: resolve(__dirname, 'public'),
     server: {
       port: 8080
